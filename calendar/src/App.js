462e5477eafa7056e0e55bb3178bd25906817437
MiyaharaHiromi <miyahara.hiromi@d-publishing.jp>
2024-05-08 10:42:09 +0900

    モーダルが開いて閉じれるように変更

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,33 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import styles from './calendar_modal.module.css'
-
+import React, { useState } from 'react';
 
 function App() {
-    // const contentCss ={
-    //   zIndex:2,
-    //   width:"50%",
-    //   padding: "1em",
-    //   background:"#fff"
-    // }
+const [show, setShow] = useState(false);
 
-  const openCalendarModal = () => {
-    return (
-      console.log("Hello world")
-    )
+  const openModal = () => {
+    return setShow(true)
   }
-  const today = new Date;
+
+  const closeModal = () => {
+    return setShow(false)
+  }
+
   return (
     <>
-   <button onClick={openCalendarModal}>このテキストをクリックでモーダルが開くようにしたい</button> 
-   <div class={styles.background}>
-    <div class={styles.contentCss}>
-      <p>これはカレンダーの中身になる予定のコンテンツ</p>
-      <p><button>close</button></p>
-    </div>
-   </div>
+      <button onClick={openModal}>このテキストをクリックでモーダルが開くようにしたい</button> 
+      <Modal show={show} closeModal={closeModal}/>
    </>
   );
+}
+
+function Modal(props){
+
+  if(props.show){
+    return (
+      <div class={styles.background}>
+        <div class={styles.contentCss}>
+          <p>これはカレンダーの中身になる予定のコンテンツ</p>
+          <p><button onClick={props.closeModal}>close</button></p>
+        </div>
+      </div>
+      )
+  }
+
 }
 
 export default App;
