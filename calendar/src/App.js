2ad7744f66e02e14d1c5e3d6143e3aa6d8a0f3cd
MiyaharaHiromi <miyahara.hiromi@d-publishing.jp>
2024-05-08 10:28:03 +0900

    モーダルの骨組みとcssを違うファイルに記述

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import styles from './calendar_modal.module.css'
+
 
 function App() {
+    // const contentCss ={
+    //   zIndex:2,
+    //   width:"50%",
+    //   padding: "1em",
+    //   background:"#fff"
+    // }
+
+  const openCalendarModal = () => {
+    return (
+      console.log("Hello world")
+    )
+  }
+  const today = new Date;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+   <button onClick={openCalendarModal}>このテキストをクリックでモーダルが開くようにしたい</button> 
+   <div class={styles.background}>
+    <div class={styles.contentCss}>
+      <p>これはカレンダーの中身になる予定のコンテンツ</p>
+      <p><button>close</button></p>
     </div>
+   </div>
+   </>
   );
 }
 
