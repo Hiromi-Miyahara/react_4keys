1446a1ffad275a85702ee09df77e0d347e472a85
MiyaharaHiromi <miyahara.hiromi@d-publishing.jp>
2024-05-15 10:59:43 +0900

    アイテムをリストに追加できるように変更　他

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,34 +1,65 @@
 import './App.css';
 import React, {useState} from 'react';
 
+function App() {
+    const [listItems, setListItems] = useState([])
 
-// TODO: state使って入力された値がリストに残るようにする
-function TodoDiv() {
-    const [todos, setTodos] = useState([])
+    function TodoDiv() {
+        const [inputText, setInputText] = useState('')
+        const resetInputText = () => {
+            return setInputText('')
+        }
 
-    const clickHandler = (e) => {
-        console.log("hello world")
+        const addListItem = () => {
+            setListItems([...listItems, inputText])
+        }
+
+        const deleteListItem = (deleteText) => {
+            setListItems(listItems.filter((item) => item !== deleteText))
+        }
+
+        const renderListItems = () => {
+            return listItems.map((listItem, index) => (
+                <div key={index}>
+                    <div className="itemText">{listItem}</div>
+                    <button onClick={() => {
+                        deleteListItem(listItem)
+                    }}>削除
+                    </button>
+                </div>
+            ));
+        };
+
+        const handleSubmit = (event) => {
+            event.preventDefault();
+        };
+        return (
+            <div className="todoDiv">
+                <form onSubmit={(e) => {
+                    handleSubmit(e)
+                }}>
+                    <input onChange={(e) => setInputText(e.target.value)} value={inputText}/>
+                    <div>"現在の入力中の文字列: {inputText}
+                        "{renderListItems()}
+                    </div>
+                    <button onClick={resetInputText}>入力中の文字列をリセット
+                    </button>
+                    <button onClick={addListItem}>追加
+                    </button>
+                </form>
+            </div>
+        )
     }
 
-    return (
-        <div className="todoDiv">
-            <form onSubmit={clickHandler}>
-                <input/>
-                <button>追加</button>
-            </form>
-        </div>
-    )
-}
+    function ListDiv() {
+        return (
+            <tr className="listDiv">
+                <td></td>
+            </tr>
+        )
+    }
 
-function ListDiv() {
-    return (
-        <tr className="listDiv">
-            <td>hello world</td>
-        </tr>
-    )
-}
 
-function App() {
     return (
         <>
             <div className="App">
